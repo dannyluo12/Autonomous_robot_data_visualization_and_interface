866c6aee6c483344e4a9f2b639e2f43fa7179edb
dannyluo12 <dannyluo66@gmail.com>
2021-02-07 03:19:32 -0800

    added additional targets to run.py

--- run.py
+++ run.py
@@ -19,6 +19,29 @@ def main(targets):
         csv_data_plots = mapping_data(**test_plots_cfg)
         # python generate_rrt_vis.py -start 100 150 -stop 350 350 -p test_track.png -s 20 # will have to try to run this
         print('')
+        
+    if 'data' in targets:
+        #getting_data = get_data(**data_cfg)
+        print('Data from project successfully pulled into ...')
+        
+    # this target might not be necessary
+    #if 'eda' in targets:
+    #    perform_eda = eda_func(gmapping_cfg)
+    #    rtab_csv_data = eda_func(**rtab_cfg)
+    #    print('Rosbag data successfully transformed into csv data and eda performed')
+        
+    if 'plot' in targets:
+        #gmapping_plot = plot_data(**gmapping_plots_cfg)
+        #rtab_plot = plot_data(**rtab_plots_cfg)
+        print('... sucessfully plotted')
+        
+    if 'all' in targets:
+        #getting_data = get_data(**data_cfg)
+        #gmapping_csv_data = eda_func(**gmapping_cfg)
+        #rtab_csv_data = eda_func(**rtab_cfg)
+        #gmapping_plot = plot_data(**gmapping_plots_cfg)
+        #rtab_plot = plot_data(**rtab_plots_cfg)
+        print('Data pipeline process of ... is successfull')
  
 if __name__ == '__main__':
     targets = sys.argv[1:]
