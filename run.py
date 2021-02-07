ed837a00477e580d934659e739ee3a94482502b7
dannyluo12 <dannyluo66@gmail.com>
2021-02-07 13:03:41 -0800

    formatting change on run.py

--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 import sys
 import json
 import os
-from src.util import run_viz
+
 sys.path.insert(0, 'src')
 
-from generate_rrt_vis import *
+from src.generate_rrt_vis import *
+from src.util import run_viz
 
 # TODO 
 # create run.py script to output results of project (make re-creatable)
@@ -16,29 +17,31 @@ def main(targets):
     #TODO
     if 'test' in targets: 
         run_viz(**mapping_cfg,**points_cfg)
-        # python generate_rrt_vis.py -start 100 150 -stop 350 350 -p test_track.png -s 20 
-        # will have to try to run line above 
-        print('Successfully ran RRT visualization on test_track.png, optimal navigation path can be viewed {}'.format(mapping_cfg['out_dir']))
+        print('Successfully ran RRT visualization on maze.png, optimal navigation path can be viewed {}'.format(mapping_cfg['out_dir']))
         
     if 'data' in targets:
-        #getting_data = get_data(**data_cfg)
+        # TODO
+        # getting_data = get_data(**data_cfg)
         pass
         # print('Data from project successfully pulled into ...')
         
     # this target might not be necessary
     if 'eda' in targets:
+    # TODO
     #    perform_eda = eda_func(gmapping_cfg)
     #    rtab_csv_data = eda_func(**rtab_cfg)
        pass
        # print('Rosbag data successfully transformed into csv data and eda performed')
         
     if 'plot' in targets:
+    # TODO
         #gmapping_plot = plot_data(**gmapping_plots_cfg)
         #rtab_plot = plot_data(**rtab_plots_cfg)
         pass
         # print('... sucessfully plotted')
         
     if 'all' in targets:
+    # TODO
         #getting_data = get_data(**data_cfg)
         #gmapping_csv_data = eda_func(**gmapping_cfg)
         #rtab_csv_data = eda_func(**rtab_cfg)
