c62c8551bf4c7a9677b708a037512134dfee3cc1
史佳 <SENSETIME\shijia@cn0314000650l.domain.sensetime.com>
2021-02-17 22:48:25 +0800

    update imu,rrt and camera part

--- src/interface/main.js
+++ src/interface/main.js
@@ -1,3 +1,42 @@
+// const { exec } = require("child_process");
+
+// exec("ls -la", (error, stdout, stderr) => {
+//     if (error) {
+//         console.log(`error: ${error.message}`);
+//         return;
+//     }
+//     if (stderr) {
+//         console.log(`stderr: ${stderr}`);
+//         return;
+//     }
+//     console.log(`stdout: ${stdout}`);
+// });
+function refresh(node)
+{
+   var times = 100; // gap in Milli Seconds;
+
+   (function startRefresh()
+   {
+      var address;
+      if(node.src.indexOf('?')>-1)
+       address = node.src.split('?')[0];
+      else 
+       address = node.src;
+      node.src = address+"?time="+new Date().getTime();
+
+      setTimeout(startRefresh,times);
+   })();
+
+}
+
+window.onload = function()
+{
+  var node = document.getElementById('img_camera');
+  refresh(node);
+  var node2 = document.getElementById('img_navi');
+  refresh(node2);
+  // you can refresh as many images you want just repeat above steps
+}
 let vueApp = new Vue({
     el: "#vueApp",
     data: {
@@ -7,7 +46,11 @@ let vueApp = new Vue({
         connected: false,
         // page content
         menu_title: 'Connection',
-        main_title: 'Main title, from Vue!!',
+        sub_title: 'RosBridge Subscribe',
+        pub_title: 'RosBridge Publish',
+        camera_title: "Input from Camera",
+        IMU_title: "Input from IMU",
+        navi_title: "RRT Nagivation"
     },
     methods: {
         connect: function() {
@@ -20,7 +63,9 @@ let vueApp = new Vue({
             this.ros.on('connection', () => {
                 this.connected = true
                 console.log('Connection to ROSBridge established!')
-                let topic = new ROSLIB.Topic({
+
+            // subsribe to topic
+            let topic = new ROSLIB.Topic({
                 ros: this.ros,
                 name: '/cmr_filtered/image',
                 messageType: 'sensor_msgs/Image'
@@ -28,6 +73,31 @@ let vueApp = new Vue({
             topic.subscribe((message) => {
                 console.log(message)
             })
+            let topic1 = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/car_1/base/odom',
+                messageType: 'nav_msgs/Odometry'
+            })
+            topic1.subscribe((message) => {
+                document.getElementById("position").innerHTML = "x: " +message.pose.pose.position.x+" y: " +message.pose.pose.position.y+" z: " +message.pose.pose.position.z;
+                document.getElementById("orientation").innerHTML = "x: " +message.pose.pose.orientation.x+" y: " +message.pose.pose.orientation.y+" z: " +message.pose.pose.orientation.z;
+                document.getElementById("angular").innerHTML = "x: " +message.twist.twist.angular.x+" y: " +message.twist.twist.angular.y+" z: " +message.twist.twist.angular.z;
+                document.getElementById("linear").innerHTML = "x: " +message.twist.twist.linear.x+" y: " +message.twist.twist.linear.y+" z: " +message.twist.twist.linear.z;
+            })
+            let topic2 = new ROSLIB.Topic({
+                ros: this.ros,
+                name: '/imu',
+                messageType: 'sensor_msgs/Imu'
+            })
+            topic2.subscribe((message) => {
+                // console.log(message)
+                document.getElementById("linear_acceleration").innerHTML = "x: " +message.linear_acceleration.x+" y: " +message.linear_acceleration.y+" z: " +message.linear_acceleration.z;
+                document.getElementById("orientation_imu").innerHTML = "x: " +message.orientation.x+" y: " +message.orientation.y+" z: " +message.orientation.z;
+                document.getElementById("angular_velocity").innerHTML = "x: " +message.angular_velocity.x+" y: " +message.angular_velocity.y+" z: " +message.angular_velocity.z;
+                
+
+            })
+
             })
             this.ros.on('error', (error) => {
                 console.log('Something went wrong when trying to connect')
