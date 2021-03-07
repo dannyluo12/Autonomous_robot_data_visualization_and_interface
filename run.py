6995d9cbc7d69de31b735e077a6871903e1cde1f
dannyluo12 <dannyluo66@gmail.com>
2021-03-06 21:17:20 -0800

    changed location of print statements, caught exception cases

--- run.py
+++ run.py
@@ -19,23 +19,23 @@ clean_file = json.load(open('config/clean_file.json'))
 def main(targets):
     if 'test' in targets: 
         run_viz(**mapping_cfg_test,**points_cfg_test)
-        print('Successfully ran RRT visualization on maze.png, optimal navigation path can be viewed {}'.format(mapping_cfg_test['out_dir']))
+        #print('Successfully ran RRT visualization on maze.png, optimal navigation path can be viewed {}'.format(mapping_cfg_test['out_dir']))
         
     if 'data' in targets:
         create_data()
-        print('Successfully created data folder in the main directory')
+        #print('Successfully created data folder in the main directory')
         
     if 'clean' in targets:
         resize(**clean_file)
-        print('Files inputted sucessfully cleaned. Should now be ready for analysis, visualization to be loaded on interactive interface')
+        #print('Files inputted sucessfully cleaned. Should now be ready for analysis, visualization to be loaded on interactive interface')
         
     if 'analyze' in targets:
         analyze(**mapping_cfg_all,**points_cfg_all)
-        print('Given navigaion algorithms have been successfully tested for performance comparison. Results can be found in the main data directory')
+        #print('Given navigaion algorithms have been successfully tested for performance comparison. Results can be found in the main data directory')
         
     if 'all' in targets:
         run_viz(**mapping_cfg_all,**points_cfg_all)
-        print('Successfully ran RRT visualization on thunderhill_cropped.png, optimal navigation path can be viewed {}'.format(mapping_cfg_all['out_dir']))
+        #print('Successfully ran RRT visualization on thunderhill_cropped.png, optimal navigation path can be viewed {}'.format(mapping_cfg_all['out_dir']))
 
 
 
